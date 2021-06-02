2f0a192f1283a065de629df064d0897841795a44
ng20001 <ng20001@byui.edu>
2021-06-02 12:27:31 +0900

    New line added by Ben

--- src/buendia/Main.java
+++ src/buendia/Main.java
@@ -3,6 +3,7 @@ package buendia;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.print("Hello, my name is Yoli");
+        System.out.println("Hello, my name is Yoli");
+        System.out.println("Hello Yoli, my name is Benjamin");
     }
 }
