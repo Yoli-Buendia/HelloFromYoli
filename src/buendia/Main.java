828a3bf3608f614499307439a42d36dc22c5830e
Yoli-Buendia <bue17003@byui.edu>
2021-06-02 23:05:40 -0500

    add a line for the stretch challenge.

--- src/buendia/Main.java
+++ src/buendia/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Yoli");
         System.out.println("Hello Yoli, my name is Benjamin");
+        System.out.println("Doing the stretch challenge");
     }
 }
